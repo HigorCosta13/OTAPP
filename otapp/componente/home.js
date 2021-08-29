21e181e00063b06702d53fbf6cabc1d704a14cdf
jhonata <jhonata.limamacedoti@gmail.com>
2021-08-29 20:39:12 -0300

    termino de layout

--- otapp/componente/home.js
+++ otapp/componente/home.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native'
-
+import { Button, StyleSheet, Text, View, SafeAreaView,TouchableOpacity,Image } from 'react-native'
+import { fonts } from 'react-native-elements/dist/config';
 import { Input } from 'react-native-elements/dist/input/Input';
+import {Divider} from 'react-native-elements'
 
 
 function home() {
@@ -11,19 +12,44 @@ function home() {
       <View style={styles.lateral}>
         <Text style={styles.textomedio}>Principais atividades</Text>
         <Button
-          onPress={''}
           title=" + Adicionar Card"
           color="#ff5618"
         />
       </View>
       <View>
-      <Input placeholder='Pesquisar'
-       leftIcon={{ type: 'font-awesome', name: 'map-marker' }}
-       />
+        <Input placeholder='Pesquisar'
+          leftIcon={{ type: 'font-awesome', name: 'map-marker' }}
+        />
       </View>
       <View >
         <Text style={styles.titulo}>Atividades</Text>
       </View>
+      <View style={styles.diviçãoDeItens}>
+      <TouchableOpacity>
+        <Image style={styles.image} source={require('../Imagens/exame.jpg')}/>
+        <Text style={styles.textomenu}>Ultimos exames</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Image style={styles.image} source={require('../Imagens/agendamento.jpg')}/>
+        <Text style={styles.textomenu}>Agendar exame</Text>
+      </TouchableOpacity>
+      </View>
+      <View style={styles.margin} >
+      <Divider/>
+      </View><View >
+        <Text style={styles.titulo}>vacinação</Text>
+      </View>
+      <View style={styles.diviçãoDeItens}>
+      <TouchableOpacity>
+        <Image style={styles.image} source={require('../Imagens/unnamed.jpg')}/>
+        <Text style={styles.textomenu}>Agendamento</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Image style={styles.image} source={require('../Imagens/age.jpeg')}/>
+        <Text style={styles.textomenu}>Proxima Dose</Text>
+      </TouchableOpacity>
+      </View>
+      
     </SafeAreaView>
 
   );
@@ -41,10 +67,32 @@ var styles = StyleSheet.create({
   },
   titulo: {
     fontSize: 30,
+    marginBottom : 10,
   },
   textomedio: {
-    fontSize: 20
+   
+  },
+  textomenu:{
+    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: 'ZCOOLKuaiLe-Regular',
+    
+  },
+  image:{
+    height: 125,
+    width: 179,
+    borderRadius:25
+  },
+  diviçãoDeItens:{
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  margin:{
+    marginBottom : 10,
+    marginTop : 25
   }
+  
+
 
 });
 

8a572b0a53e412087cdc1621514fa0d4ce3c5015
jhonata <jhonata.limamacedoti@gmail.com>
2021-09-15 21:49:01 -0300

    mds como ele errou?????

--- otapp/componente/agendamento.js
+++ otapp/componente/agendamento.js
@@ -1,14 +1,229 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'; 
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,ScrollView } from 'react-native';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Picker } from '@react-native-picker/picker';
+
+LocaleConfig.locales['br'] = {
+  monthNames: ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"],
+  monthNamesShort: ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"],
+  today: "Hoje",
+  dayNames: ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta"],
+  dayNamesShort: ["D","S","T","Q","Q","S","S"]
+}
+
+LocaleConfig.defaultLocale = "br"
 
 function agendamento({ navigation }) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>agendamento</Text>
-      </View>
-    );
-  }
-  
 
-  export default agendamento;
+
+  
+  const [especialidades] = useState(['Acupuntura', 'Alergia e imunologia', 'Anestesiologia', 'Angiologia', 'Cardiologia', 'Cirurgia cardiovascular','Cirurgia da mão', 'Cirurgia de cabeça e pescoço', 'Cirurgia do aparelho digestivo', 'Cirurgia geral', 'Cirurgia oncológica', 'Cirurgia pediátrica', 'Cirurgia plástica', 'Cirurgia torácica', 'Cirurgia vascular', 'Clínica médica', 'Coloproctologia', 'Dermatologia', 'Endocrinologia e metabologia', 'Endoscopia', 'Gastroenterologia', 'Genética médica', 'Geriatria', 'Ginecologia e obstetrícia', 'Hematologia e hemoterapia', 'Homeopatia', 'Infectologia', 'Mastologia', 'Medicina de emergência', 'Medicina de família e comunidade', 'Medicina do trabalho', 'Medicina de tráfego', 'Medicina esportiva', 'Medicina física e reabilitação', 'Medicina intensiva', 'Medicina legal e perícia médica', 'Medicina nuclear', 'Medicina preventiva e social', 'Nefrologia', 'Neurocirurgia', 'Neurologia', 'Nutrologia', 'Oftalmologia', 'Oncologia clínica', 'Ortopedia e traumatologia', 'Otorrinolaringologia', 'Patologia', 'Patologia clínica/medicina/laboratorial', 'Pediatria', 'Pneumologia', 'Psiquiatria', 'Radiologia e diagnóstico por imagem', 'Radioterapia', 'Reumatologia', 'Urologia'])
+  
+  const [especialidadeSelecionada, setEspecialidadeSelecionada] = useState([])
+  
+  const [hospitais] = useState(['Hospital Israelita Albert Einstein', 'Hospital Albert Sabin', 'Hospital Alvorada', 'Hospital Aviccena', 'Hospital Leforte', 'Hospital Beneficência Portuguesa', 'Hospital CEMA', 'Hospital Central de Guaianases', 'Hospital do Coração', 'Hospital Edmundo Vasconcelos', 'Hospital e Maternidade Metropolitano', 'Hospital Jardins', 'Hospital Nipo-Brasileiro', 'Hospital 9 de Julho', 'Hospital Alemão Oswaldo Cruz'])
+  
+  const [hospitalSelecionado, setHospitalSelecionado] = useState([])
+  
+  const [horarios] = useState(['08:00', '09:00', '11:30', '15:00', '16:30', '17:00'])
+  
+  const [horarioSelecionado, setHorarioSelecionado] = useState([])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+      <View style={styles.lateral}>
+        <Text style={styles.titulo}>Agendamento</Text>
+      </View>
+        
+      <View style={styles.subtitulo}>
+        <Text style={styles.textomedio}>Confira os dias disponíveis</Text>
+      </View>
+        
+      <View style={styles.subtitulo2}>
+        <Text style={styles.textomedio2}>Selecione o dia</Text>
+      </View>
+        
+      <View style={{marginTop: 80}}>
+        <>
+          <Calendar minDate={"2021-09-01"} onDayPress={(e) => {
+            console.log('e', e)
+            }}
+            theme={{
+              todayTextColor: '#ff5618',
+              selectedDayBackgroundColor: '#00adf5',
+              textSectionTitleColor: '#ff5618',
+              arrowColor: '#ff5618',
+            }}
+          />
+        </>
+      </View>
+
+
+
+      <View>
+        <Text style={styles.select}>Especialidade:</Text>
+          <Picker
+            selectedValue={especialidadeSelecionada}
+            style={{height: 50, width: 200, marginLeft: 100, marginTop: -40, alignSelf: 'flex-end'}}
+            onValueChange={(itemValue) => setEspecialidadeSelecionada(itemValue)}>{ 
+              especialidades.map(es => {
+              return <Picker.Item label={es} value={es} key={es} />})}
+          </Picker>
+      </View>
+        
+      <View>
+        <Text style={styles.select2}>Hospital:</Text>
+          <Picker selectedValue={hospitalSelecionado} style={{height: 50, width: 200, marginLeft: 100, marginTop: -40, alignSelf: 'flex-end'}} 
+            onValueChange={(itemValue) => setHospitalSelecionado(itemValue)}>{ 
+            hospitais.map(hos => {
+            return <Picker.Item label={hos} value={hos} key={hos} />})}
+          </Picker>
+      </View>
+
+
+      <View>
+        <Text style={styles.select2}>Horários Díponiveis:</Text>
+          <Picker selectedValue={horarioSelecionado} style={{height: 50, width: 200, marginLeft: 100, marginTop: -40, alignSelf: 'flex-end'}} 
+            onValueChange={(itemValue) => setHorarioSelecionado(itemValue)}>{
+            horarios.map(ho => {return <Picker.Item label={ho} value={ho} key={ho}  />})}
+          </Picker>
+      </View>
+        
+      <View style={styles.avisos}>
+        <Text style={styles.aviso1}>
+          • Chegar com 15 minutos de antecedência
+        </Text>
+        <Text style={styles.aviso2}>
+          • Portar máscara cirurgica (medidas anti Covid-19)
+        </Text>
+        <Text style={styles.aviso2}>
+          • Portar cartão Smart HealthPass ou Telefone Celular
+        </Text>
+        <Text style={styles.aviso2}>
+          • Manter o distanciamento de pelo menos 2 metros
+        </Text>
+      </View>
+      
+      <View style={styles.botaoarea}>
+        <TouchableOpacity
+          style={styles.btncancelar} >
+          <Text style={styles.btntext}>Cancelar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnagendar} >
+          <Text style={styles.btntext}>Agendar</Text>
+        </TouchableOpacity>
+      </View>
+      </ScrollView>
+    </SafeAreaView>
+
+  );
+}
+
+var styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: 'white',
+    height: '100%',
+  },
+  lateral: {
+    justifyContent: 'space-between',
+    margin: 10,
+    flexDirection: 'row',
+  },
+  titulo: {
+    margin: 10,
+    fontSize: 40,
+    marginBottom: 10,
+    fontFamily: 'Poppins-Bold',
+    fontWeight: 'bold',
+  },
+  subtitulo: {
+    justifyContent: 'space-between',
+    marginTop: 90,
+    margin: 10,
+    flexDirection: 'row',
+    position: 'absolute',
+    marginLeft: 30,
+  },
+  subtitulo2: {
+    justifyContent: 'space-between',
+    marginTop: 150,
+    margin: 10,
+    flexDirection: 'row',
+    position: 'absolute',
+    marginLeft: 175,
+  },
+  select: {
+    fontSize: 25,
+    marginLeft: 20,
+    marginTop: 40,
+    color: '#000000',
+    fontWeight: 'bold',
+  },
+  select2: {
+    fontSize: 25,
+    marginLeft: 20,
+    marginTop: 10,
+    color: '#000000',
+    fontWeight: 'bold',
+  },
+  avisos: {
+    marginTop: 35,
+  },
+  aviso1: {
+    fontSize: 19,
+    color: 'gray',
+    marginLeft: 0,
+  },
+  aviso2: {
+    fontSize: 19,
+    color: 'gray',
+    marginLeft: 0,
+    marginTop: 2,
+  },
+  textomedio: {
+    fontSize: 20,
+    color: 'gray',
+  },
+  textomedio2: {
+    fontSize: 20,
+    color: 'black',
+  },
+  botaoarea: {
+    fontSize: 25,
+    marginLeft: 20,
+    marginTop: 40,
+    color: '#000000',
+    fontWeight: 'bold',
+    flexDirection: 'row',
+  },
+  btnagendar: {
+    alignItems: 'center',
+    backgroundColor: '#ff5618',
+    padding: 20,
+    borderRadius: 10,
+    zIndex: 10,
+    width: 200,
+    marginLeft: 10,
+  },
+  btncancelar: {
+    alignItems: 'center',
+    backgroundColor: '#ff5618',
+    padding: 20,
+    borderRadius: 10,
+    zIndex: 10,
+    width: 200,
+    marginRight: 10,
+  },
+  btntext: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+  }
+
+});
+
+export default agendamento;
--- otapp/componente/perfil.js
+++ otapp/componente/perfil.js
@@ -1,14 +1,227 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'; 
+import React from 'react';
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+} from 'react-native';
 
-function perfil({ navigation }) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>perfil</Text>
+const App = () => {
+
+  return (
+    <View style={styles.container}> 
+      <View style={styles.photo}>
+        <View>
+        <Image source={require('../Imagens/depositphotos_37819499-stock-photo-old-man-smoking.jpg')} style={{
+          resizeMode: "stretch",
+          width:"100%",
+          height: "100%",
+        }}
+        />
+        </View>
       </View>
+      <View style={styles.info}>
+        <View style={styles.nameSection}>
+          <Text style={styles.name}>Porsche 911 GT2 RS</Text>
+        </View>
+        <View style={styles.sepBar}>
+        </View>
+        <View style={styles.categories}>
+          <Pressable style={styles.categoryButton}>
+            <Text style={styles.categoryText}>Sobre</Text>
+          </Pressable>
+          <Pressable style={styles.categoryButton}>
+            <Text style={styles.categoryText}>Exames</Text>
+          </Pressable>
+          <Pressable style={styles.categoryButton}>
+            <Text style={styles.categoryText}>Agendar</Text>
+          </Pressable>
+        </View>
+        <View style={styles.ageCate}>
+          <View>
+            <Text style={styles.cateItemText}>Idade</Text>
+            <Text style={styles.cateItemValue}>55 Anos</Text>
+          </View>
+          <View>
+            <Text style={styles.cateItemText}>Nascimento</Text>
+            <Text style={styles.cateItemValue}>10/05/1966 </Text>
+          </View>
+        </View>
+        <View style= {styles.minDetails}> 
+            <Text style= {styles.minDetailsText}>{'\u25CF'} Cardiomiopatia Hipertrófica</Text>
+            <Text style= {styles.minDetailsText}>{'\u25CF'} Alérgio a AAS (Ácido acetilsalicílico)</Text>
+            <Text style= {styles.minDetailsText}>{'\u25CF'} Último exame realizado em 2019</Text>
+            <Text style= {styles.minDetailsText}>{'\u25CF'} Pendência de vacinação - Covid-19</Text>
+        </View>
+        <View>
+          <Pressable  onPress={()=> Alert.alert("Za Waruuudo!")} style={styles.moreInfoButton}>
+            <Text style={styles.MoreInfoText}>Mais Informações</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
     );
-  }
-  
+};
 
-  export default perfil;
+const styles = StyleSheet.create({
+  container:  {
+    flexDirection: "column",
+    backgroundColor: "#FFFF",
+  },
+  photo:{
+    flex: 0,
+    height: "40%",
+  },
+  
+  info: {
+    flex:0,
+    height:"60%",
+    backgroundColor: "#FBFBFB",
+    borderStartWidth:2,
+    borderTopRightRadius: 24,
+    borderTopLeftRadius: 24,
+    borderTopStartRadius:24,
+    borderTopEndRadius:24,
+    borderEndWidth:2,
+
+  },
+
+  nameSection: {
+    marginTop:5,
+    paddingTop:15,
+    justifyContent:'center',
+    flexDirection:'row',
+  },
+  name:{
+    flexDirection: "row",
+    justifyContent: "center",
+    fontWeight: "bold",
+    fontFamily: "Poppins",
+    fontSize:28,
+    color:"#2B2B2B",
+  },
+  categories:{
+    flexDirection:"row",
+    marginTop:10,
+    marginBottom:20,
+    justifyContent: "center",
+  },
+  categoryButton:{
+    paddingTop: 10,
+    paddingBottom:10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontFamily: "Poppins",
+    fontSize:13,
+    borderRadius: 12,
+    marginLeft:15,
+    marginRight: 15,
+    backgroundColor: "#F5F5F5",
+  },
+
+  categoryButtonActive:{
+    paddingTop: 10,
+    paddingBottom:10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontFamily: "Poppins",
+    fontSize:13,
+    borderRadius: 12,
+    marginLeft:15,
+    marginRight: 15,
+    backgroundColor: "#2B2B2B",
+  },
+
+  categoryText: {
+    fontSize:16,
+    fontWeight:"bold",
+    color: "#B0B0B0",
+    fontFamily: "poppins",
+  },
+
+  categoryTextActive: {
+    fontSize:16,
+    fontWeight:"bold",
+    color: "#FCFCFC",
+    fontFamily: "poppins",
+  },
+
+  ageCate: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginBottom:10,
+    alignContent: "center",
+  },
+
+  cateItemText:{
+    fontFamily: "Poppins",
+    fontSize: 17,
+    paddingLeft:30,
+    color: "#B0B0B0",
+  },
+  cateItemValue:{
+    fontFamily: "Poppins",
+    fontSize: 18,
+    paddingLeft: 30,
+    color:"#2B2B2B",
+},
+
+minDetails:{
+
+  paddingTop:10,
+  paddingLeft:15,
+  paddingRight:15,
+  paddingBottom:10,
+},
+
+minDetailsText:{
+  flexDirection: "row",
+  paddingTop:5,
+  paddingLeft:8,
+  paddingRight:8,
+  paddingBottom:5,
+  color:"#B0B0B0",
+  fontSize:16,
+},
+moreInfoButton: {
+  width: "50%",
+  marginTop:20,
+  marginBottom:5,
+  marginLeft:100,
+  borderRadius: 12,
+  paddingTop:10,
+  paddingBottom:10,
+  paddingRight:12,
+  paddingLeft:12,
+  backgroundColor: "#FF9345",
+},
+
+MoreInfoText:{
+  textAlign: "center",
+  fontWeight: "bold",
+  fontFamily: "Poppins",
+  color: "#FCFCFC",
+  fontSize:16,
+},
+
+sepBar: {
+  marginTop:20,
+  paddingTop: 0.1,
+  borderTopColor: "#E8E8E8",
+  borderBottomColor: "#E8E8E8",
+  borderWidth:2,
+  marginRight: 20,
+  marginLeft: 20,
+  borderRadius:48,
+},
+
+});
+
+export default App;

067f2dbd023e65bd0bd2ab714a12081dc5458f18
jhonata <jhonata.limamacedoti@gmail.com>
2021-11-17 23:04:20 -0300

    add

--- otapp/componente/login.js
+++ otapp/componente/login.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { SafeAreaView, View } from 'react-native';
 import { Button, Card, Text, TextInput } from 'react-native-paper';
 import { loginStyle } from '../styles/login.style';
-
+import { Alert} from 'react-native'
 
 function login({ navigation }) {
+    const [email, setEmail] = React.useState('');
+    const [senha, setSenha] = React.useState('');
     return (
-
+        
         <SafeAreaView style={loginStyle.content}>
-           
+
             <View style={loginStyle.view}>
                 <Card>
                     <Card.Content>
@@ -19,18 +21,23 @@ function login({ navigation }) {
                             <Text style={loginStyle.text2}>Entre com seu e-mail e senha</Text>
                         </View>
 
-                        <TextInput label="E-mail" keyboardType="email-address" style={loginStyle.textInput1}></TextInput>
-                        <TextInput label="Senha de acesso" secureTextEntry={true} style={loginStyle.textInput2}></TextInput>
-                        <Button mode="contained" style={loginStyle.button} onPress={() => console.log('Pressed')}>
+                        <TextInput label="E-mail" keyboardType="email-address"  value={email}  onChangeText={text => setEmail(text)} style={loginStyle.textInput1}></TextInput>
+                        <TextInput label="Senha de acesso" secureTextEntry={true}  value={senha}  onChangeText={text => setSenha(text)}  style={loginStyle.textInput2}></TextInput>
+                        <Button mode="contained" style={loginStyle.button} onPress={() => Alert.alert(
+                                                                                        'Alert Title',
+                                                                                         'Email '+ email +' , ' + 'senha ' + senha , 
+                                                                                        [
+                                                                                            { text: 'Cancel', onPress: () => console.log('Cancel Pressed!') },
+                                                                                         
+                                                                                        ],
+                                                                                        { cancelable: false }
+                                                                                    )}>
                             <Text style={loginStyle.textButton}>Entrar</Text>
-                        </Button>
-                        <Button uppercase={false} onPress={() => console.log('Pressed')} style={loginStyle.senha}>
-                            <Text>Esqueceu a senha?</Text>
                         </Button>
                         <View>
                             <Text style={loginStyle.text3}>OU</Text>
                         </View>
-                        <Button mode="contained" style={loginStyle.button} onPress={()=> navigation.navigate('cadastro',{name:'cadastro'})} >
+                        <Button mode="contained" style={loginStyle.button} onPress={() => navigation.navigate('cadastro', { name: 'cadastro' })} >
                             <Text style={loginStyle.textButton}>Cadastrar</Text>
                         </Button>
                     </Card.Content>
